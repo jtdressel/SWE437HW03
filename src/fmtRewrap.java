117a80d5f47634225750afc1d9766f3f4a0f6a72
James Dressel <ubergeek@jamesdressel.com>
2012-02-10 17:30:19 -0500

    FEATURE: Add columns to fmtRewrap
    Satisfy the testFmtRewrapLongSentence test, which makes an input longer than the
    width.

--- src/fmtRewrap.java
+++ src/fmtRewrap.java
@@ -2,7 +2,34 @@
 //James Dressel
 public class fmtRewrap {
 	static public String fmtRewrap(String input, int width){
-		StringBuilder build = new StringBuilder(input);
+		StringBuilder build = new StringBuilder();
+		StringBuilder line = new StringBuilder();
+		String[] words = input.split(" ");
+		
+
+		for(String word:words){
+			int projectedLength = line.length()+word.length()+1;
+			
+			if(line.length()==0){
+				//nothing in the line
+				line.append(word);
+			} else if(projectedLength<width){
+				line.append(" ");
+				line.append(word);
+			} else{
+				build.append(line.toString());
+				build.append("\n");
+				line = new StringBuilder(word);
+				
+			}
+			
+			
+		} 
+		
+		build.append(line.toString());
+		
+		
+		
 		build.append("\n");
 		return build.toString();
 	}
